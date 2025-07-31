7714dd27ce902c3094b70982c0cc3546e59a9c93
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2025-07-31 19:50:36 -0300

    Projeto configurado

---
+++ src/test/java/cepein/cepein_atividade2/CepeinAtividade2ApplicationTests.java
@@ -0,0 +1,16 @@
+package cepein.cepein_atividade2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepeinAtividade2ApplicationTests
+{
+
+    @Test
+    void main()
+    {
+        CepeinAtividade2Application.main(new String[] {});
+    }
+
+}
