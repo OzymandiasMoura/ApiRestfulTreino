73c9862da46fa9403422962ebd85769ee4f95698
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2025-08-06 15:21:46 -0300

    Atendente.findById teste service criado

--- src/test/java/cepein/cepein_atividade2/services/impl/AtendenteServiceImplTest.java
+++ src/test/java/cepein/cepein_atividade2/services/impl/AtendenteServiceImplTest.java
@@ -1,20 +1,61 @@
 package cepein.cepein_atividade2.services.impl;
 
+import cepein.cepein_atividade2.domain.Atendente;
+import cepein.cepein_atividade2.domain.dto.AtendenteDto;
+import cepein.cepein_atividade2.repositories.AtendenteRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AtendenteServiceImplTest
 {
+    @InjectMocks
+    AtendenteServiceImpl service;
+
+    @Mock
+    AtendenteRepository repository;
+
+    private Optional<Atendente> atendenteOptional;
+    private AtendenteDto atendenteDto;
+    private Atendente atendente;
+
+    private final static Integer id = 1;
+    private final static String nome = "Pedro";
+    private final static String cpf = "11111111111";
 
     @BeforeEach
     void setUp()
     {
+        MockitoAnnotations.openMocks(this);
+        startAtendentes();
     }
 
     @Test
-    void findById()
+    void whenFindByIdReturnAtendente()
     {
+        Mockito.when(repository.findById(id)).thenReturn(atendenteOptional);
+
+        Atendente response = service.findById(id);
+
+        assertNotNull(response);
+        assertEquals(Atendente.class, response.getClass());
+        assertEquals(id, response.getIdAtendente());
+        assertEquals(nome, response.getNome());
+        assertEquals(cpf, response.getCpf());
+    }
+
+    private void startAtendentes()
+    {
+        atendente = new Atendente(id, nome, cpf);
+        atendenteDto = new AtendenteDto(id, nome, cpf);
+        atendenteOptional = Optional.of(new Atendente(id, nome, cpf));
     }
 }
