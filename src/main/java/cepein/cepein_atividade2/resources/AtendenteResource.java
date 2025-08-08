3a65c18ac6034a96bda0a28b8f5105db84f2b22b
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2025-08-08 14:45:08 -0300

    AtendenteResource criado

--- src/main/java/cepein/cepein_atividade2/resources/AtendenteResource.java
+++ src/main/java/cepein/cepein_atividade2/resources/AtendenteResource.java
@@ -5,10 +5,12 @@ import cepein.cepein_atividade2.domain.dto.AtendenteDto;
 import cepein.cepein_atividade2.services.AtendenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/atendente")
@@ -22,5 +24,47 @@ public class AtendenteResource
     {
         Atendente atendente = service.findById(id);
         return ResponseEntity.ok().body(new AtendenteDto(atendente.getIdAtendente(), atendente.getNome(), atendente.getCpf(), atendente.getAtivo()));
+    }
+
+    @PostMapping
+    public ResponseEntity<AtendenteDto> createAtendente(@RequestBody AtendenteDto atendenteDto)
+    {
+        Atendente atendente = service.create(atendenteDto);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(atendente.getIdAtendente()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AtendenteDto>> findAll()
+    {
+        List<AtendenteDto> atendentes = new ArrayList<>();
+        service.findAll().stream().map(atendente -> atendentes.add(new AtendenteDto(atendente.getIdAtendente(), atendente.getNome(), atendente.getCpf(), atendente.getAtivo())));
+
+        return ResponseEntity.ok().body(atendentes);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<AtendenteDto> update(@PathVariable Integer id, @RequestBody AtendenteDto atendenteDto)
+    {
+        Atendente atendente = service.update(atendenteDto);
+        return ResponseEntity.ok().body(new AtendenteDto(atendente.getIdAtendente(), atendente.getNome(), atendente.getCpf(), atendente.getAtivo()));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<AtendenteDto> delete(@PathVariable Integer id)
+    {
+        Atendente atendente = service.findById(id);
+        AtendenteDto atendenteDto = new AtendenteDto(atendente.getIdAtendente(), atendente.getNome(), atendente.getCpf(), atendente.getAtivo());
+        service.delete(atendenteDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/delete/{id}")
+    public ResponseEntity<AtendenteDto> softDelete(@PathVariable Integer id,  @RequestBody AtendenteDto atendenteDto)
+    {
+        Atendente atendente = service.softDelete(atendenteDto);
+        return ResponseEntity.ok().body(new AtendenteDto(atendente.getIdAtendente(), atendente.getNome(), atendente.getCpf(), atendente.getAtivo()));
+
     }
 }
