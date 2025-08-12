38b038e5025f88d3577f896d15217ba646ba2ee8
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2025-08-12 15:28:59 -0300

    ProdutoMapper criado

--- src/main/java/cepein/cepein_atividade2/repositories/ProdutoRepository.java
+++ src/main/java/cepein/cepein_atividade2/repositories/ProdutoRepository.java
@@ -2,7 +2,9 @@ package cepein.cepein_atividade2.repositories;
 
 import cepein.cepein_atividade2.domain.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>
 {
 
