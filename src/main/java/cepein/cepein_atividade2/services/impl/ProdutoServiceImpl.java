a71502a40fc60afa4146f0fe2639fda39d6da7d1
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2025-08-12 15:29:32 -0300

    ProdutoService.findById criado

--- src/main/java/cepein/cepein_atividade2/services/impl/ProdutoServiceImpl.java
+++ src/main/java/cepein/cepein_atividade2/services/impl/ProdutoServiceImpl.java
@@ -2,27 +2,33 @@ package cepein.cepein_atividade2.services.impl;
 
 import cepein.cepein_atividade2.domain.Produto;
 import cepein.cepein_atividade2.domain.dto.ProdutoDto;
+import cepein.cepein_atividade2.domain.mapper.ProdutoMapper;
 import cepein.cepein_atividade2.repositories.ProdutoRepository;
 import cepein.cepein_atividade2.services.ProdutoService;
+import cepein.cepein_atividade2.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class ProdutoServiceImpl implements ProdutoService
 {
+    private final String notFoundException = "Produto não encontrado";
+
     @Autowired
     private ProdutoRepository repository;
 
     @Override
     public Produto findById(Integer id)
     {
-        return null;
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(notFoundException));
     }
 
     @Override
-    public Produto create(ProdutoDto atendente)
+    public Produto create(ProdutoDto dto)
     {
-        return null;
+        Produto produto = ProdutoMapper.dtoToEntity(dto);
+        return repository.save(produto);
     }
 
     @Override
