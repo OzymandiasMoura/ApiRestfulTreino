3187f8c4d38157be47b67c183725f34c900590c7
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2025-08-15 11:31:35 -0300

    ProdutoResourceTest configurado

--- src/test/java/cepein/cepein_atividade2/resources/ProdutoResourceTest.java
+++ src/test/java/cepein/cepein_atividade2/resources/ProdutoResourceTest.java
@@ -1,16 +1,39 @@
 package cepein.cepein_atividade2.resources;
 
+import cepein.cepein_atividade2.domain.Produto;
+import cepein.cepein_atividade2.domain.dto.ProdutoDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProdutoResourceTest
 {
+    private Optional<Produto> produtoOptional;
+    private ProdutoDto produtoDto;
+    private Produto produto;
+
+    private final static Integer id = 1;
+    private final static String nome = "Geladeira";
+    private final static String barCode = "1111111111111";
+    private final static Double preco = 1550.50;
+    private final static Boolean ativo = true;
+
+    public static final String notFoundMessage = "Produto não encontrado!";
+    public static final String dataIntegrityMessage = "Código de barras já existe no sistema!";
+    public static final String invalidFormatMessage = "Formato do código de barras é invalido.";
 
     @BeforeEach
     void setUp()
     {
+        MockitoAnnotations.openMocks(this);
+
+        produto = new Produto(id,  nome, barCode, preco, ativo);
+        produtoOptional = Optional.of(new Produto(id,  nome, barCode, preco, ativo));
+        produtoDto = new ProdutoDto(id,  nome, barCode, preco, ativo);
     }
 
     @Test
