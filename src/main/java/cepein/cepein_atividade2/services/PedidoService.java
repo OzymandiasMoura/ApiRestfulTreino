2e9258a6ef866da9d242ec5ee3e83ba92bafdc95
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2025-08-19 11:35:51 -0300

    PedidoService criado

--- src/main/java/cepein/cepein_atividade2/services/PedidoService.java
+++ src/main/java/cepein/cepein_atividade2/services/PedidoService.java
@@ -1,6 +1,16 @@
 package cepein.cepein_atividade2.services;
 
+import cepein.cepein_atividade2.domain.Pedido;
+import cepein.cepein_atividade2.domain.dto.PedidoDto;
+
+import java.util.List;
+
 public interface PedidoService
 {
-
+    Pedido findById(Integer id);
+    Pedido create(PedidoDto dto);
+    List<Pedido> findAll();
+    Pedido update(PedidoDto dto);
+    void delete(Integer id);
+    Pedido closeOrder(PedidoDto dto);
 }
--- src/main/java/cepein/cepein_atividade2/services/impl/PedidoServiceImpl.java
+++ src/main/java/cepein/cepein_atividade2/services/impl/PedidoServiceImpl.java
@@ -1,10 +1,71 @@
 package cepein.cepein_atividade2.services.impl;
 
+import cepein.cepein_atividade2.domain.Pedido;
+import cepein.cepein_atividade2.domain.dto.PedidoDto;
+import cepein.cepein_atividade2.domain.mapper.PedidoMapper;
+import cepein.cepein_atividade2.repositories.PedidoRepository;
 import cepein.cepein_atividade2.services.PedidoService;
+import cepein.cepein_atividade2.services.exceptions.DataIntegrityException;
+import cepein.cepein_atividade2.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PedidoServiceImpl implements PedidoService
 {
+    @Autowired
+    PedidoRepository repository;
 
+    @Override
+    public Pedido findById(Integer id)
+    {
+        Optional<Pedido> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Pedido não encontrado!"));
+    }
+
+    @Override
+    public Pedido create(PedidoDto dto)
+    {
+        validationOrder(dto);
+        return repository.save(PedidoMapper.dtoToEntity(dto));
+    }
+
+    @Override
+    public List<Pedido> findAll()
+    {
+        return repository.findAll();
+    }
+
+    @Override
+    public Pedido update(PedidoDto dto)
+    {
+        validationOrder(dto);
+        return repository.save(PedidoMapper.dtoToEntity(dto));
+    }
+
+    @Override
+    public void delete(Integer id)
+    {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Pedido closeOrder(PedidoDto dto)
+    {
+        Pedido obj = PedidoMapper.dtoToEntity(dto);
+        obj.fecharPedido();
+        return repository.save(obj);
+    }
+
+    void validationOrder(PedidoDto dto)
+    {
+        Optional<Pedido> obj =  repository.findById(dto.getIdPedido());
+        if(obj.isPresent() && !(obj.get().getIdPedido().equals(dto.getIdPedido())))
+        {
+            throw new DataIntegrityException("Pedido já criado");
+        }
+    }
 }
