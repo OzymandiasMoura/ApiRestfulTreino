db00170dfccd2c6b5c58f0f604f74310ed7f761d
OzymandiasMoura <pedro.hcmoura11@gmail.com>
2025-09-03 12:25:30 -0300

    Venda criado

---
+++ src/main/java/cepein/cepein_atividade2/domain/mapper/VendaMapper.java
@@ -0,0 +1,19 @@
+package cepein.cepein_atividade2.domain.mapper;
+
+import cepein.cepein_atividade2.domain.Venda;
+import cepein.cepein_atividade2.domain.dto.VendaDto;
+
+public class VendaMapper
+{
+    public static VendaDto entityToDto(Venda v)
+    {
+        VendaDto dto = new VendaDto(v.getIdVenda(), v.getProduto(), v.getPedido(), v.getQuantidade(), v.getDesconto(), v.getPrecoFinal());
+        return dto;
+    }
+
+    public static Venda dtoToEntity(VendaDto dto)
+    {
+        Venda entity = new Venda(dto.getIdVenda(), dto.getProduto(), dto.getPedido(), dto.getQuantidade(), dto.getDesconto(), dto.getPrecoFinal());
+        return entity;
+    }
+}
---
+++ src/main/java/cepein/cepein_atividade2/services/VendaService.java
@@ -0,0 +1,8 @@
+package cepein.cepein_atividade2.services;
+
+import cepein.cepein_atividade2.domain.Venda;
+
+public interface VendaService
+{
+    Venda create();
+}
